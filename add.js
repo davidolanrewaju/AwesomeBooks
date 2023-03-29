5631cb9fb94517e6c4006d6dfad1deb390ee6152
David <davidolanrewaju538@gmail.com>
2023-03-29 22:41:22 +0100

    Update add.js to add and remove books from list and local storage

--- add.js
+++ add.js
@@ -1,5 +1,6 @@
 const bookLists = document.querySelector('.book-lists');
 const form = document.getElementById('form');
+const listOfBooks = 'bookLists';
 
 function updateList(event) {
   event.preventDefault();
@@ -13,7 +14,29 @@ function updateList(event) {
     </div>
   `;
 
+  // Save books to local storage
+  localStorage.setItem(listOfBooks, JSON.stringify(bookLists.innerHTML));
+
   form.reset();
 }
 
+// Load books from local storage
+function loadBooks() {
+  const savedBooks = JSON.parse(localStorage.getItem(listOfBooks));
+  if (savedBooks) {
+    bookLists.innerHTML = savedBooks;
+  }
+}
+
+// Remove book from list and local storage
+function removeBook(event) {
+  if (event.target.classList.contains('remove')) {
+    const bookItem = event.target.parentElement;
+    bookLists.removeChild(bookItem);
+    localStorage.setItem(listOfBooks, JSON.stringify(bookLists.innerHTML));
+  }
+}
+
+bookLists.addEventListener('click', removeBook);
+window.addEventListener('load', loadBooks);
 form.addEventListener('submit', updateList);
